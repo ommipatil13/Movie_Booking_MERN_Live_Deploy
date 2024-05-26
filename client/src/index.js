60e112ce4d463862cbce384fea9736606bff693d
Omkar Patil <ommipatil13@gmail.com>
2024-05-26 13:12:28 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.baseURL = "http://localhost:8080/"
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
